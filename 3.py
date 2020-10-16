6842a6296038bc6d924d4d762c7da3a8a858b5dd
tongri <antohagryb@gmail.com>
2020-10-16 23:55:13 +0300

    s

--- 3.py
+++ 3.py
@@ -1,21 +1,19 @@
-russia = {
-	"XXS": 42,
-	"XS": 44,
-	"S": 46,
-	"M": 48,
-	"L": 50,
-	"XL": 52,
-	"XXL": 54,
-	"XXXL": 56,
-}
-def convertation(russia):
-	required = input("Enter the size to convert: ").upper()
-	claimed = russia[required]
-	print("In russia it is " + str(claimed))
-	print("In Germany it is " + str(claimed - 6))
-	print("In the USA it is " + str(claimed -34))
-	print("In France it is " + str(claimed - 4))
-	print("In Great Britain it is " + str(claimed - 18))
+import re
 
+with open("2.txt", "r") as f:
+	with open("3.txt", "w") as t:
+		for line in f:
+			print ("\n")
+			pattern = re.compile("[0-9]+")
+			result = pattern.findall(line)
+			t.write("\n")
+			for x in range(1, int(result[2]) + 1):
+				if not x % int(result[0]) and not x % int(result[2]):
+					t.write("FB ")
 
-convertation(russia)
+				elif not x % int(result[0]):
+					t.write("F ")
+				elif not x % int(result[1]):
+					t.write("B ")
+				else:
+					t.write(str(x) + " ")
