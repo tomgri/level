89796f43ecf80f65b2ba5eb03b0e28ef7c0c8e82
tongri <antohagryb@gmail.com>
2020-10-17 11:05:58 +0300

    nu

--- new.py
+++ new.py
@@ -3,7 +3,6 @@ quantity = len(number)
 copy = number
 number = int(number)
 word = set()
-som = ["а если тут", "mb da", "прием с мака", "отправка с мака"]
 
 for rank in range(1, quantity):
 	dis = copy[rank:quantity]
